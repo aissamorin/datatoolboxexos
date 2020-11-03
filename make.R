95786b80c1bb11f9c945202e1191bfb8f2602469
MORIN AISSA p1818244 <aissa.morin@univ-lyon1.fr>
2020-11-03 16:32:43 +0100

    :t-rex: + Make

---
+++ make.R
@@ -0,0 +1,18 @@
+# #############################################################
+#
+#Data and Code for the CESAB Datatoolbox Exercises
+#
+#########################################################
+
+
+# ----- clean workspace
+rm(list = ls())
+
+# ----- install/update packages
+devtools::install_deps()
+
+# ----- install compendium package
+devtools::install(build = FALSE)
+
+# ----- Knit exo dplyr
+rmarkdown::render(here::here("exercices","exo_dplyr.Rmd"))
